57c10770441cff3b0875eb5a9877e04197e189f1
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-05-19 10:08:21 -0500

    Creando el archivo recitaCliente dentro de pub ,poema.txt dentro de priv ,y el archivo indexResuelto2.js en el lado del servidor

---
+++ pub/recitaCliente.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',function(){
+	document.getElementById("prueba").innerHTML="Este texto se inserto con un Script dentro de pub ";	
+	return false;	
+});
+
+
+
+
+
+ 
