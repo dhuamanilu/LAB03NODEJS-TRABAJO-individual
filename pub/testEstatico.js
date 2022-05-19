26d52263eae148f073e997fbc31fa20521554e26
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-05-19 10:02:23 -0500

    Ejercico resuelto 1 completo

--- pub/testEstatico.js
+++ pub/testEstatico.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
-	document.querySelector("#prueba").innerHTML="Este texto se inserto con un Script dentro de pub ";	
+	document.getElementById("prueba").innerHTML="Este texto se inserto con un Script dentro de pub ";	
 	return false;	
 });
 
