4cd7a90160b3df3088104693d411b80da6b23630
Diego Huamani <dhuamanilu@unsa.edu.pe>
2022-05-19 09:58:29 -0500

    Creando el archivo testEstatico.js dentro de la carpeta pub,ademas puse un div en el html para modificarlo con el testEsrtatico.js

---
+++ pub/testEstatico.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',function(){
+	document.querySelector("#prueba").innerHTML="Este texto se inserto con un Script dentro de pub ";	
+	return false;	
+});
+
+
+
+
+
+ 
